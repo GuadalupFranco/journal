cdbba4ac4f305a634690028c397e3750ec1d7809
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-11-23 14:41:45 -0600

    add bootstrap-icons via npm

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Laravel Journal</title>
 </head>
